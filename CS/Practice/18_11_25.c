778fe7118d2ea9cad46a9e5091bab1379575d32d
MaroHuh <marohuhh@gmail.com>
2025-11-18 18:54:24 +0200

    practice

---
+++ CS/Practice/18_11_25.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    /*
+    char declaration[100] = "Bitch, this chicken is cold.";
+    for (int i = 0; i <= strlen(declaration) - 1; i++)
+    {
+        printf("%d:  %c \n", i + 1, declaration[i]);
+    }
+     */
+    // --------------------------------------------------------------------------------------------------------------
+    /*
+        int x, y = 1;
+        printf("All even numbers between x and y\n");
+        while(1)
+        {
+            scanf("%d %d", &x, &y);
+            if (y < x)
+            {
+                printf("Y needs to be bigger than x.");
+                continue;
+            }
+            for (int i = x; i <= y; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    printf("%d \n", i);
+                }
+            }
+            break;
+        }
+        printf("All even numbers between %d and %d", x, y);
+    */
+    // -----------------------------------------------------------------------------------------------------------------
+}

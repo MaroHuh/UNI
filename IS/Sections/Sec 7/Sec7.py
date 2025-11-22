5986bc9cd79a1e3c08a5169dc7b6b57c917935e3
Omar Tarek <marohuhh@gmail.com>
2025-11-22 15:16:03 +0200

    Sec7

--- IS/Sections/Sec 7/Sec7.py
+++ IS/Sections/Sec 7/Sec7.py
@@ -20,5 +20,6 @@ def counter(x,y):
         two = two + 1
     print(f"There are {count} numbers between {one} and {two_orig}")
 
-counter(5,10)
-
+#counter(5,10)
+#counter(100,50)
+#counter(100,1245)
